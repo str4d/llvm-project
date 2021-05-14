Bevinsky/llvm-gbz80

GBZ80: Prevent MachineCSE from coalescing small regclasses.
Ugh. This is a huge hack, but it solves some
register allocation issues.

lib/CodeGen/MachineCSE.cpp
@@ -152,7 +152,7 @@ bool MachineCSE::PerformTrivialCopyPropagation(MachineInstr *MI,
     if (DefMI->getOperand(1).getSubReg())
       continue;
     const TargetRegisterClass *RC = MRI->getRegClass(Reg);
-    if (!MRI->constrainRegClass(SrcReg, RC))
+    if (!MRI->constrainRegClass(SrcReg, RC, 2))
       continue;
     DEBUG(dbgs() << "Coalescing: " << *DefMI);
     DEBUG(dbgs() << "***     to: " << *MI);

str4d/llvm-project

llvm/lib/CodeGen/MachineCSE.cpp
@@ -195,7 +195,7 @@ bool MachineCSE::PerformTrivialCopyPropagation(MachineInstr *MI,
     // class given a super-reg class and subreg index.
     if (DefMI->getOperand(1).getSubReg())
       continue;
-    if (!MRI->constrainRegAttrs(SrcReg, Reg))
+    if (!MRI->constrainRegAttrs(SrcReg, Reg, 2))
       continue;
     LLVM_DEBUG(dbgs() << "Coalescing: " << *DefMI);
     LLVM_DEBUG(dbgs() << "***     to: " << *MI);